af405782e38c8aef59ebe67dc3f3d5c20c66ef00
709macdonald <709macdonald@gmail.com>
2025-03-03 10:14:00 -0330

    updated server

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const { l } = require("vite/dist/node/types.d-aGj9QkWt");
 dotenv.config();
 const cors = require("cors");
+const connectDB = require("./db/db");
 const port = process.env.PORT;
 
 const app = express();
 
 app.use(cors());
+``;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+connectDB();
+
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
-``;
