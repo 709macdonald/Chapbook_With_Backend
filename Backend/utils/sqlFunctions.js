b950b4e32d2ab0fd844a0762943d9ae7e609409d
709macdonald <709macdonald@gmail.com>
2025-03-03 10:29:52 -0330

    createTable function

---
+++ Backend/utils/sqlFunctions.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql");
+const config = require("../db/config");
+const pool = mysql.createPool(config);
+
+const createTable = (schema) => {
+  return new Promise((resolve, reject) => {
+    pool.query(schema, (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+};
